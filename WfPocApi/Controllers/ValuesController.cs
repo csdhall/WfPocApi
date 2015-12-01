60ae66d4b7d0dd69636ce8a5b38f40eb56bd2033
csdhall <csdhall@chanderdhall.com>
2015-12-01 14:35:17 -0600

    Made API work for MVC.

--- WfPocApi/Controllers/ValuesController.cs
+++ WfPocApi/Controllers/ValuesController.cs
@@ -14,15 +14,21 @@ namespace WfPocApi.Controllers
         // GET api/values
         public Task<HttpResponseMessage> Get()
         {
+            //This calls WebForms
             HttpClient client = new HttpClient();
             return client.GetAsync("http://localhost:2558/");
             //http://localhost:2558/
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Task<HttpResponseMessage> Get(int id)
         {
-            return "value";
+            //This calls MVC
+            HttpClient client = new HttpClient();
+            var baseUri = string.Format("{0}://{1}", Request.RequestUri.Scheme,
+                Request.RequestUri.Authority);
+            return client.GetAsync(baseUri + "/home/index");
+
         }
 
         // POST api/values
